18f906da4522807fa755a22b7ebf833f885dbeb0
Arielle Gironza <ariellegironza@Arielles-MacBook-Air.local>
2023-06-14 15:59:44 -0500

    setting up your database

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 ///////////// INITIAL SETUP
-// require express
-const express = require("express")
+// require("dotenv").config()
+const express = require("express") // to require express
+const drinks = require("./models/drinks") // to access the drinks "database"
+// const PORT = process.env.PORT // to get the PORT from .env file
 const app = express()
 
 ///////////// ROUTES
 app.get("/", (req, res) => {
     res.send('Welcome to the Gitpub App!');
+})
+
+app.get("/drinks", (req, res) => {
+    res.send(drinks);
 })
 
 
